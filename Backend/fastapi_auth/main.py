3513be4f6bce86c679978cb993273cf84d5ebef8
Favio <favio.vives.ruidias@gmail.com>
2025-09-03 17:22:02 -0500

    CasiV1.1.1

--- Backend/fastapi_auth/main.py
+++ Backend/fastapi_auth/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(auth.router, tags=["auth"])
 
 @app.get("/")
 def root():
